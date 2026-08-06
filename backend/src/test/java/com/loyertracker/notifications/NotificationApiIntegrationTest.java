94160aa22e832884f668076b5f4269efb39e2d18
jordan <jptshilombo@gmail.com>
2026-08-06 15:23:46 +0100

    test(notifications): aligner le format US-125

--- backend/src/test/java/com/loyertracker/notifications/NotificationApiIntegrationTest.java
+++ backend/src/test/java/com/loyertracker/notifications/NotificationApiIntegrationTest.java
@@ -18,7 +18,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
